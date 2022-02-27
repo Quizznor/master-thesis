8b2043ab92f3f8f5e0f504ab9b050d5a7599610f
Paul Filip <filip@iklx288.ikp.kit.edu>
2022-02-27 11:31:11 +0100

    Update trace heatmaps

--- scripts/ExtractTraceCharacteristics/view_trace_heatmap.py
+++ scripts/ExtractTraceCharacteristics/view_trace_heatmap.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 22})
 
 
-background = np.loadtxt("/cr/users/filip/data/accumulated_background.csv")
-signal = np.loadtxt("/cr/users/filip/data/accumulated_signal.csv")
+background = np.loadtxt("/cr/users/filip/data/first_simulation/heatmap/accumulated_background.csv")
+signal = np.loadtxt("/cr/users/filip/data/first_simulation/heatmap/accumulated_signal.csv")
 
 background = np.log(background)
 background[background == -np.inf] = 0
@@ -16,18 +16,18 @@ signal[signal == -np.inf] = 0
 
 # heatmap for background
 plt.figure()
-plt.title("Background trace heatmap")
-background_heatmap = plt.imshow(background, aspect = 'auto', origin = 'lower')
-plt.colorbar(background_heatmap, label="absolute occurence")
-plt.ylabel("Signal strength (au)")
+plt.title("Background trace heatmap, 20281 events")
+background_heatmap = plt.imshow(background, aspect = 'auto', origin = 'lower', extent = [0, 2048, 0, 22])
+plt.colorbar(background_heatmap, label="log(absolute occurence)")
+plt.ylabel("Signal strength (VEM)")
 plt.xlabel("Time bin (8.3 ns)")
  
 # heatmap for signal
 plt.figure()
-plt.title("Signal trace heatmap")
-signal_heatmap = plt.imshow(signal, aspect = 'auto', origin = 'lower')
-plt.colorbar(signal_heatmap, label="absulute occurence")
-plt.ylabel("Signal strength (au")
+plt.title("Signal trace heatmap, 20281 events")
+signal_heatmap = plt.imshow(signal, aspect = 'auto', origin = 'lower', extent = [0, 2048, 0, 600])
+plt.colorbar(signal_heatmap, label="log(absolute occurence)")
+plt.ylabel("Signal strength (VEM)")
 plt.xlabel("Time bin (8.3 ns)")
 
 plt.show()
