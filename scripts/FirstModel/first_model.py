fb7e3f57bccaf1412d8ade134ed4c5df229b984c
Paul Filip <filip@iklx288.ikp.kit.edu>
2022-03-03 16:03:27 +0100

    Send first model to training

--- scripts/FirstModel/first_model.py
+++ scripts/FirstModel/first_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from mimetypes import init
 import typing
 import sys, os
 import numpy as np
@@ -11,149 +12,146 @@ class VEMTrace():
 
     def __init__(self, trace_length : int, mu : float = 0, std : float = 1) -> None :
 
-        self._array = np.random.normal(mu, std, trace_length)
+        self.__array = np.random.normal(mu, std, trace_length)
 
-    # overlay signal trace at random position of baseline
+    # overlay signal trace at uniformly random position of baseline
     def __iadd__(self, signal : np.array) -> object :
         assert trace_length > len(signal), "SIGNAL DOES NOT FIT INTO BASELINE!\n"
-        start = np.random.randint(- trace_length, -len(signal))
-        self._array[start : start + len(signal)] += signal
+
+        start = np.random.randint(-trace_length, -len(signal))
+        self.__array[start : start + len(signal)] += signal
 
         return self
 
     # default getter for baseline trace
     def get(self) -> np.array : 
-        return self._array
+        return self.__array
 
-class Dataset():
+# Generator class for NN sequential model with some additional functionalities
+# This website helped tremendously with writing a working example: shorturl.at/fFI09
+class TraceGenerator(tf.keras.utils.Sequence):
 
-    def __init__(self, fixed_seed : bool = True, shuffle : bool = False):
+    def __init__(self, train : bool, split : float, input_shape : int, fix_seed : bool = False, shuffle : bool = True) -> None:
 
-        # make random numbers predictable if desired
-        fixed_seed and self.set_random_number_generator_seed(0)
+        assert 0 < split and split < 1, "PLEASE PROVIDE A VALID SPLIT: 0 <= split <= 1"
 
-        self.fixed_seed = fixed_seed    # set RNG seed for reproducible results
-        self.shuffle = shuffle          # preshuffle signal/background generators
+        self.train = train              # wether object is used for training or validation
+        self.split = split              # 0.00 - 1.00, percentage of data for this generator
+        self.input_shape = input_shape  # input length of the data, trace duration = 8.3 ns * length
+        self.fix_seed = fix_seed        # whether or not RNG seed is fixed, for reproducibility
+        self.shuffle = shuffle          # whether or not to shuffle signal files at the end of each epoch
 
-        # set filesystem binaries
-        self._working_directory = "/cr/users/filip/data/first_simulation/tensorflow/signal/"
-        self._signal_files = os.listdir(self._working_directory)
+        self.n_events = [0, 0]          # number of background/signal traces respectively
         
-        # generate the dataset
-        self.__dataset = tf.data.Dataset.from_generator(self._generator, (tf.float32, tf.int32), ( (20000,), ()))
+        self.__working_directory = "/cr/users/filip/data/first_simulation/tensorflow/signal/"
+        self.__signal_files = os.listdir(self.__working_directory)
 
-    # prevent entire dataset from being loaded into memory via generators
-    def _generator(self) -> typing.Generator[tuple, None, None] :
+    # generator that creates one batch, loads one signal file (~2000 events)
+    # and randomly shuffles background events inbetween the signal traces
+    def __getitem__(self, index) -> tuple :
 
-        # TODO Provide a way of looking at progress in stdout
+        self.__traces, self.__labels = [], []
 
-        #########################
-        # background labels == 0
-        # signal labes      == 1
-        #########################
+        # generate mixed set of signals and baselines
+        events = np.loadtxt(self.__working_directory + self.__signal_files[index])
 
-        # generate signal events first, then baselines
-        # definitely works, but memory expensive shuffling
-        if not self.shuffle:
+        for signal in events:
 
-            # around 200k station-level traces available
-            for file in self._signal_files:
-                events = np.loadtxt(self._working_directory + file)
-                for signal in events:
-                    Baseline = VEMTrace(trace_length)
-                    Baseline += signal
-                    yield Baseline.get(), 1
+            # decide whether baseline gets interlaced
+            choice = np.random.choice([0,1])
 
-            # Exactly n_signal background datapoints
-            for step in range(n_signal):
-                yield VEMTrace(trace_length).get(), 0
-        
-        # generate already mixed set of signals and baselines
-        # more memory friendly, but needs to be tested 
-        elif self.shuffle:
-            for file in self._signal_files:
-                events = np.loadtxt(self._working_directory + file)
-                for signal in events:
+            while not choice:
+                self.add_event(choice, signal)      # add background event to list
+                choice = np.random.choice([0,1])    # add another background event?
+            self.add_event(choice, signal)          # add signal event to list
 
-                    # decide whether baseline gets interlaced
-                    choice = np.random.choice([0,1])
+        # true batch size is not EXACTLY batch size, this should be okay
+        return (np.array(self.__traces), np.array(self.__labels))
 
-                    while not choice:
-                        yield VEMTrace(trace_length).get(), 0
-                        choice = np.random.choice([0,1])
+    # have 100 signal files => number of steps per epoch = 100
+    # such that optimizer sees all data over an entire epoch
+    def __len__(self):
+        return 100
 
-                    Baseline = VEMTrace(trace_length)
-                    Baseline += signal
-                    yield Baseline.get(), 1
+    # add labelled trace consisting of either just baseline or baseline + signal             
+    def add_event(self, choice : int, signal : np.ndarray) -> tuple:
 
-    # return dataset main attribute
-    def get(self):
-        return self.__dataset
+        self.n_events[choice] += 1
+        Baseline = VEMTrace(trace_length)
+        if choice == 1: Baseline += signal
 
-    # seed RNG for reproducibility
+        choice_encoded = tf.keras.utils.to_categorical(choice, 2)
+        self.__traces.append(Baseline.get())
+        self.__labels.append(choice_encoded)
+
+    # called by model.fit at the end of each epoch
+    def on_epoch_end(self) -> None : 
+        self.shuffle and np.random.shuffle(self.__signal_files)
+
+    # fix random number generator seed for reproducibility
     @staticmethod
     def set_random_number_generator_seed(seed : int = 0) -> None :
         np.random.seed(seed)
 
-    # no idea exactly what this does, see shorturl.at/dhpyQ for more info
-    def get_inputs(self, n_shuffle : int, n_batch : int) -> tuple :
+# Wrapper for tf.keras.Sequential model with some additional functionalities
+# TODO add __predict_batch, __predict_trace functionalities
+class Classifier():
 
-        # (re)set batch and shuffle size, just to be sure
-        global shuffle_size, batch_size
-        shuffle_size = n_shuffle
-        batch_size   = n_batch
+    def __init__(self, init_from_disk : str = None) -> None:
+
+        self.epochs = 0
+        self.model = tf.keras.models.Sequential()
+
+        # architecture of this NN is - apart from in/output - completely arbitrary, at least for now
+        # self.model.add(tf.keras.layers.Input(shape=(trace_length, ), dtype = tf.float32, name = "Input"))         # Input layer, trace_length neurons
+        self.model.add(tf.keras.layers.Dense(units = 2048, input_shape=(trace_length,), activation = 'relu'))       # 1st hidden layer 2048 neurons
+        self.model.add(tf.keras.layers.Dropout(0.2))                                                                # Dropout layer to fight overfitting
+        self.model.add(tf.keras.layers.Dense(units = 12, activation = 'relu'))                                      # 2nd hidden layer 12 neurons
+        self.model.add(tf.keras.layers.Dropout(0.2))                                                                # Dropout layer to fight overfitting
+        self.model.add(tf.keras.layers.Dense(units = 2, activation = 'softmax', name = "Output"))                   # Output layer with signal/background
         
-        # shuffle and group the dataset if needed
-        if not self.shuffle:
-            events = events.shuffle(shuffle_size)
+        if init_from_disk is not None:
+            self.epochs = int(init_from_disk[init_from_disk.rfind('_') + 1:init_from_disk.rfind('.')])              # set previously run epochs as start
+            self.model = tf.keras.models.load_model(init_from_disk)                                                 # load model, doesn't work with h5py 3.x !!
+        
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        events = events.batch(batch_size)
-        events = events.prefetch(1)
+    # Train the model network on the provided training/validation set
+    def train(self, training_set : TraceGenerator, validation_set : TraceGenerator, epochs : int, verbose : int = 0) -> None:
+        
+        # clear terminal for better overview of verbose output:
+        verbose and print("\x1B[2J\x1B[H")
 
-        features, labels = events.make_one_shot_iterator().get_next()
-        return features, labels
+        self.epochs += epochs
+        self.model.fit(training_set, validation_data=validation_set, epochs = epochs, verbose = verbose)
 
-    # takes roughly YES amount of time and RAM (=
-    def show_data(self, n_shuffle : int, n_batch : int) -> None:
+    # Save the model to disk
+    def save(self, directory_path : str) -> None : 
+        self.model.save(directory_path + f"first_model_{self.epochs}.SavedModel")
 
-        features, labels = self.get_inputs(n_shuffle, n_batch)
+    # Predict a batch or single trace
+    def predict(self):
+        pass                # TODO
 
-        with tf.Session() as session:
-            f_data, l_data = session.run([features, labels])
-            for trace, label in zip(f_data, l_data):
-
-                print("Preparing file...")
-
-                color = "r" if label == 1 else "b"
-                layer = 0 if label == 1 else 1
-                plt.rcParams.update({'font.size': 22})
-                plt.ylabel("Signal strength (VEM)")
-                plt.xlabel("Time bin (8.3 ns)")
-
-                plt.plot(range(len(trace)), trace, c = color, zorder = layer)
-
-                plt.plot([],[], c = "r", label="Signal traces")
-                plt.plot([],[], c = "b", label="Background traces")
-                plt.legend()
-
-                plt.savefig("/cr/users/filip/plots/first_model_test.png")
+    # Wrapper for pretty printing
+    def __str__(self) -> str :
+        self.model.summary()
+        return ""
 
 if __name__=="__main__":
-
-    import matplotlib.pyplot as plt
 
     # Have 206563 station-level traces in dataset from first_simulation, see <check_signal_size.py> 
 
     n_signal = 206563               # total number of station-level traces in dataset
-    shuffle_size = 2 * n_signal     # buffer size for shuffling, should be O(2 * n_signal)
     trace_length = 20000            # total trace "duration", 8.3 ns/bin * 20 000 bins = 166 μs
-    batch_size = 1000               # number of points in batch for (e.g) gradient descent
 
-    # initialize dataset (this doesn't load it into memory yet!)
-    VirtualEventDataset = Dataset(shuffled=True)
-    VirtualEventDataset.show_data()
+    # initialize datasets (this doesn't load them into memory yet!)
+    VirtualTrainingSet = TraceGenerator(train = True, split = 0.8, input_shape = trace_length, fix_seed = True)
+    VirtualValidationSet = TraceGenerator(train = True, split = 0.2, input_shape = trace_length, fix_seed = True)
 
+    # initialize convolutional neural network model
+    SignalBackgroundClassifier = Classifier()
 
-    # initialize CNN model
-    # TODO
-
+    # train the classifier and save it to disk
+    SignalBackgroundClassifier.train(VirtualTrainingSet, VirtualValidationSet, 100, 1)
+    SignalBackgroundClassifier.save("/cr/users/filip/data/first_simulation/tensorflow/model/")
---
+++ scripts/FirstModel/first_model_wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /cr/data01/hahn/envs/tfenv/bin/activate
+python /cr/users/filip/scripts/FirstModel/first_model.py
