590555f8a1cb3d8e229dcbdc787741f2a7fe2dbf
Paul Filip <filip@iklx288.ikp.kit.edu>
2022-09-10 13:30:52 +0200

    Clean up directories

--- Trigger/Test/test_classifiers.py
+++
@@ -1,4 +0,0 @@
-from TriggerStudyBinaries_v7 import *
-
-HardwareTriggers = HardwareClassifier()
-HardwareTriggers.production_test(100000, apply_downsampling = True)
--- Trigger/plot_trace.py
+++
@@ -1,7 +0,0 @@
-from TriggerStudyBinaries_v5 import *
-
-Events = EventGenerator(["17_17.5"], split = 1, force_inject = 1)
-
-traces, labels = Events.__getitem__(2, full_trace = True)
-
-traces[0].__plot__()
